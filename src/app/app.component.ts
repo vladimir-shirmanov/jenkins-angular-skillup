f05c07e29009441b134cb12a1a4680984ebc5e77
vladimir <vladimir.shirmanov@gmail.com>
2020-10-21 23:43:02 +0300

    some fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { version } from '../../package.json'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'jen-tut';
+  title = 'jen-tut ci cd is working';
   version = version;
 }
